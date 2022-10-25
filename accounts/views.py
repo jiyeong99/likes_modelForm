63ac31dc7cf8e92634b1f9cec3d3b2df86b0df2b
jiyeong99 <1999wldud@gmail.com>
2022-10-25 17:55:19 +0900

    20221025

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 
@@ -20,7 +21,7 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
-
+@require_safe
 def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
     context = {
@@ -60,13 +61,16 @@ def update(request):
     }
     return render(request, 'accounts/update.html', context)
 
-@login_required
+@require_POST
 def follow(request,pk):
-    fuser = get_user_model().objects.get(pk=pk)
-    if request.method == 'POST':
-        if fuser.followers.filter(pk=request.user.pk).exists():
+    if request.user.is_authenticated:
+        fuser = get_object_or_404(get_user_model(), pk=pk)
+        if request.user == fuser:
+            messages.warning(request,'자기자신 팔로우 불가')
+            return redirect('accounts:detail', pk)
+        if request.user in fuser.followers.all():
             fuser.followers.remove(request.user)
         else:
             fuser.followers.add(request.user)
-        return redirect('accounts:detail', pk)
-    return redirect('accounts:detail', pk)
+        return redirect('accounts:detail',pk)
+    return redirect('accounts:login')
